4edc80a2f08ea73f4511ff5e09d77a303ad06528
Wojtek2810 <75879048+Wojtek2810@users.noreply.github.com>
2023-09-13 19:46:14 +0200

    Wyświetlanie kodu Morse'a na podstawie podanej litery.

--- main.py
+++ main.py
@@ -11,7 +11,30 @@ with open('morse_code_chart.csv', mode='r') as file:
     for row in data:
         letter_to_morse_code.append(row)
 
-#Utworzenie dwóch list w pętlach:
+#Utworzenie dwóch list przy pomocy pętli:
 #Alfabet:
+alphabet = []
+for row in letter_to_morse_code:
+    alphabet.append(row[0])
 
-print(letter_to_morse_code)
+#Morse Code:
+morse_code = []
+for row in letter_to_morse_code:
+    morse_code.append(row[1])
+
+
+#Znalezienie danej litery w liście:
+def letter_to_morse_code(letter):
+    index = 0
+    for i in alphabet:
+        if i == letter:
+            return index
+        index += 1
+
+
+while True:
+    letter = input("Podaj zdanie do przetłumaczenia na kod Morse'a").upper()
+    try:
+        print(f'{letter} - {morse_code[letter_to_morse_code(letter)]}')
+    except TypeError:
+        print("Zastosowano niedozwolony znak")
