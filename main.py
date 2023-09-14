4edce44340919a69880873de23592438bc5f4c52
Wojtek2810 <75879048+Wojtek2810@users.noreply.github.com>
2023-09-14 08:27:21 +0200

    Wersja z możliwością wielokrotnych tłumaczeń.

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import csv
 
-# Otwarcie pliku CSV w trybie odczytu
+# Otwarcie pliku CSV w trybie do odczytu
 with open('morse_code_chart.csv', mode='r') as file:
     # Utworzenie obiektu czytnika CSV
     data = csv.reader(file)
 
-    #Lista z kodem morse'a
+    # Lista z kodem morse'a
     letter_to_morse_code = []
     # Przetwarzanie danych w pętli
     for row in data:
         letter_to_morse_code.append(row)
 
-#Utworzenie dwóch list przy pomocy pętli:
-#Alfabet:
+# Utworzenie dwóch list przy pomocy pętli:
+# Alfabet:
 alphabet = []
 for row in letter_to_morse_code:
     alphabet.append(row[0])
 
-#Morse Code:
+# Morse Code:
 morse_code = []
 for row in letter_to_morse_code:
     morse_code.append(row[1])
 
 
-#Znalezienie danej litery w liście:
+# Znalezienie danej litery w liście:
 def letter_to_morse_code(letter):
     index = 0
     for i in alphabet:
@@ -31,25 +31,46 @@ def letter_to_morse_code(letter):
             return index
         index += 1
 
-#Komunikaty dla urzytkownika:
-print(f'Można korzystać z następujących znaków:\n{alphabet}')
-sentence = input("Podaj zdanie do przetłumaczenia na kod Morse'a:\n").upper()
 
-#Rozbicie zdania na litery:
-letters = []
-for letter in sentence:
-    letters.append(letter)
+# Rozbicie zdania na litery:
+def sentence_to_letters(sentence):
+    letters = []
+    for letter in sentence:
+        letters.append(letter)
+    return letters
 
-#Tłumaczenie zdania litera po literze na kod Morse'a, wyświetlanie komunikatów:
+
+# Komunikaty dla użytkownika:
+print("Tłumacz zdań na kod Morse'a.\n")
+print(f'Można korzystać z następujących znaków:\n{alphabet}\n')
+sentence = input("Jeśli chcesz wyjść, naciśniej ENTER. Jeśli chcesz tłumaczyć, podaj zdanie i zaakceptuj:\n").upper()
+
+# Tłumaczenie zdania litera po literze na kod Morse'a, wyświetlanie komunikatów:
 sentence_in_morse_code = ""
 unpermitted_sign = ""
-try:
-    for letter in letters:
-        unpermitted_sign = letter
-        sentence_in_morse_code = sentence_in_morse_code + morse_code[letter_to_morse_code(letter)]
-except TypeError:
-    print(f"Zastosowano niedozwolony znak: {unpermitted_sign}")
-else:
-    print(f"Podane zdanie przetłumaczone na kod Morse'a:\n{sentence_in_morse_code}")
 
-input("Wciśnij Enter, aby zakończyć...")
+if sentence == "":
+    runnning = False
+else:
+    runnning = True
+
+while runnning:
+    sentence_in_morse_code = ""
+    unpermitted_sign = ""
+    letters = sentence_to_letters(sentence)
+    try:
+        for letter in letters:
+            unpermitted_sign = letter
+            sentence_in_morse_code = sentence_in_morse_code + morse_code[letter_to_morse_code(letter)]
+    except TypeError:
+        print(f"\nZastosowano niedozwolony znak: {unpermitted_sign}\n")
+        sentence = input(
+            "Jeśli chcesz wyjść, naciśniej ENTER. Jeśli chcesz tłumaczyć, podaj zdanie i zaakceptuj:\n").upper()
+        if sentence == "":
+            runnning = False
+    else:
+        print(f"\nPodane zdanie przetłumaczone na kod Morse'a:\n{sentence_in_morse_code}\n")
+        sentence = input(
+            "Jeśli chcesz wyjść, naciśniej ENTER. Jeśli chcesz tłumaczyć, podaj zdanie i zaakceptuj:\n").upper()
+        if sentence == "":
+            runnning = False
